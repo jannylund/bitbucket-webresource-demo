49ab9b50e9a84e0b55c68493921d07bf5ef18452
Jan Nylund <jan.nylund@wartsila.com>
2018-07-22 23:30:05 +0200

    Implemented Condition and made it work in IntelliJ

--- src/main/java/fi/apio/api/MyPluginComponent.java
+++
@@ -1,6 +0,0 @@
-package fi.apio.api;
-
-public interface MyPluginComponent
-{
-    String getName();
-}
--- src/main/java/fi/apio/conditions/IsPluginLicensedCondition.java
+++ src/main/java/fi/apio/conditions/IsPluginLicensedCondition.java
@@ -1,21 +1,28 @@
 package fi.apio.conditions;
 
+import java.util.Map;
+
+import com.atlassian.plugin.web.Condition;
 import com.atlassian.plugin.webresource.condition.SimpleUrlReadingCondition;
 
-public class IsPluginLicensedCondition extends SimpleUrlReadingCondition
-{
+
+public class IsPluginLicensedCondition extends SimpleUrlReadingCondition implements Condition {
+
     @Override
-    protected boolean isConditionTrue()
-    {
-        return false;
+    protected boolean isConditionTrue() {
+        return true;
     }
 
     @Override
-    protected String queryKey()
-    {
+    protected String queryKey() {
         // This string will be appended to URLs as a GET parameter
         // whenever 'isConditionTrue' returns true.
         // You should make the string short, but unique to your app.
         return "myPluginName";
+    }
+
+    @Override
+    public boolean shouldDisplay(Map<String, Object> map) {
+        return true;
     }
 }
