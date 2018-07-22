89288a9681cb3c713bc04f148db3d894dd1a6a18
Jan Nylund <jan.nylund@wartsila.com>
2018-07-23 00:47:40 +0200

    Try to inject PluginLicenseManager.

--- src/main/java/fi/apio/conditions/IsPluginLicensedCondition.java
+++ src/main/java/fi/apio/conditions/IsPluginLicensedCondition.java
@@ -2,11 +2,23 @@ package fi.apio.conditions;
 
 import java.util.Map;
 
+import javax.inject.Inject;
+
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.plugin.web.Condition;
 import com.atlassian.plugin.webresource.condition.SimpleUrlReadingCondition;
+import com.atlassian.upm.api.license.PluginLicenseManager;
 
 
 public class IsPluginLicensedCondition extends SimpleUrlReadingCondition implements Condition {
+
+    @ComponentImport
+    private final PluginLicenseManager pluginLicenseManager;
+
+    @Inject
+    public IsPluginLicensedCondition(final PluginLicenseManager pluginLicenseManager) {
+        this.pluginLicenseManager = pluginLicenseManager;
+    }
 
     @Override
     protected boolean isConditionTrue() {
@@ -23,6 +35,10 @@ public class IsPluginLicensedCondition extends SimpleUrlReadingCondition impleme
 
     @Override
     public boolean shouldDisplay(Map<String, Object> map) {
-        return true;
+        if (pluginLicenseManager.getLicense().isDefined()) {
+            return pluginLicenseManager.getLicense().get().isValid();
+        } else {
+            return false;
+        }
     }
 }
